e40d97704c6abce669a442a2708556790596cf3a
Daniel Ramirez <zoerunner@gmail.com>
2021-12-09 15:20:03 -0800

    Scaffold project

---
+++ miniature_painters/miniature_painters/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for miniature_painters project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'miniature_painters.settings')
+
+application = get_asgi_application()
